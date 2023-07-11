25367cc6900d7ff9d09faa856a3b2ac60f4fd55f
Aiden Sato <aiden@aekai.dev>
2023-07-11 13:22:27 -0700

    add support for setting memory allocation, use aikar flags

--- launcher.py
+++ launcher.py
@@ -6,6 +6,28 @@ import argparse
 import subprocess
 
 API_ENDPOINT = "https://api.papermc.io/v2/projects/paper"
+AIKAR = [
+    "-XX:+UseG1GC",
+    "-XX:+ParallelRefProcEnabled",
+    "-XX:MaxGCPauseMillis=200",
+    "-XX:+UnlockExperimentalVMOptions",
+    "-XX:+DisableExplicitGC",
+    "-XX:+AlwaysPreTouch",
+    "-XX:G1NewSizePercent=30",
+    "-XX:G1MaxNewSizePercent=40",
+    "-XX:G1HeapRegionSize=8M",
+    "-XX:G1ReservePercent=20",
+    "-XX:G1HeapWastePercent=5",
+    "-XX:G1MixedGCCountTarget=4",
+    "-XX:InitiatingHeapOccupancyPercent=15",
+    "-XX:G1MixedGCLiveThresholdPercent=90",
+    "-XX:G1RSetUpdatingPauseTimePercent=5",
+    "-XX:SurvivorRatio=32",
+    "-XX:+PerfDisableSharedMem",
+    "-XX:MaxTenuringThreshold=1",
+    "-Dusing.aikars.flags=https://mcflags.emc.gs",
+    "-Daikars.new.flags=true",
+]
 
 
 def fetch(url, headers={}):
@@ -27,12 +49,19 @@ parser.add_argument(
     "--filename",
     help="filename of papermc jar (overwritten if not skipping download)",
 )
+parser.add_argument("-m", "--memory", help="memory to allocate (defaults to 4G)")
 parser.add_argument(
     "--skip-download", help="use currently downloaded papermc jar", action="store_true"
 )
 parser.add_argument(
     "--skip-execution", help="don't run jar after download", action="store_true"
 )
+parser.add_argument(
+    "--skip-aikar",
+    help="don't use aikar's flags (https://docs.papermc.io/paper/aikars-flags)",
+    action="store_true",
+)
+parser.add_argument("--gui", help="show the gui", action="store_true")
 
 args = vars(parser.parse_args())
 filename = args["filename"] if args["filename"] else "papermc.jar"
@@ -67,7 +96,14 @@ with open("eula.txt", "w") as f:
     f.write("eula=true")
 
 if not args["skip_execution"]:
+    memory = args["memory"] or "4G"
     try:
-        subprocess.run(["java", "-jar", filename])
+        subprocess.run(
+            ["java"]
+            + ([f"-Xms{memory}", f"-Xmx{memory}"])
+            + ([] if args["skip_aikar"] else AIKAR)
+            + ["-jar", filename]
+            + ([] if args["gui"] else ["--nogui"])
+        )
     except KeyboardInterrupt:
         print("done!")
